cf6fc7142e9d2ef9841970c1c11d0612f04cd20f
ahmadalmuhtar <ahmadalmuhtar@gmail.com>
2024-02-17 11:23:11 +0100

    fixed meta title and description:

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,10 @@
 import React from 'react'
 import './App.css'
-import { Helmet } from 'react-helmet'
 import { HomePage } from './pages'
-
-const HemletWrapper = () => (
-    <Helmet>
-        <meta charSet="utf-8" />
-        <title>Mario Putzen Reinigungsdienst</title>
-        <meta
-            name="description"
-            content="die beste Reinigungsdienst in Konstanz und Essen"
-        />
-    </Helmet>
-)
 
 const App = () => {
     return (
         <>
-            <HemletWrapper />
             <HomePage />
         </>
     )
